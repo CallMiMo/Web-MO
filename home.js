36252d00a75645d1a6b5dc6bde443d7014ab8246
Modesire <callmimo14@gmail.com>
2025-08-27 13:26:42 +0100

    Partially created a wireframe!

--- home.js
+++ home.js
@@ -23,11 +23,59 @@
     // console.log(`The Square Of ${n} Is ${n*n}.`)
 // }
 // findSquare(500)
-const anchors = document.querySelectorAll('#slider-btns > a')
-for(const a of anchors){
-    a.addEventListener('click', () => {
-        anchors.forEach((anc) => anc.classList.remove('on'))
-        a.classList.add('on');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const anchors = document.querySelectorAll('#slider-btns > a')
+// for(const a of anchors){
+//     a.addEventListener('click', () => {
+//         anchors.forEach((anc) => anc.classList.remove('on'))
+//         a.classList.add('on');
         // console.log(a.getAttribute('href'))
+//     })
+// }
+// const int = [2,4,6]
+// for(const a of int)console.log(a*a,a*a*a)
+//     // console.log(int.at(-3))
+//     // ARRAY METHODS
+//     int.push(8,10,12)
+//     int.unshift(-2,-4)
+//     int.pop()
+//     int.shift()
+//     console.log(int.at(-4))
+//     console.log(int.includes(-2))
+//     console.log(int.indexOf(10))
+//     int.push(100,200)
+//     console.log(int.length)
+// console.log(colors[0],colors[1],colors[2])
+// console.log(color[1])
+// console.log(color[2])
+document.querySelectorAll('.img').forEach(img => {
+    img.addEventListener('click' , (e) => {
+        document.querySelector('aside').classList.add('view');
     })
-}
+})
+document.querySelector('button.close').addEventListener('click' , (e) => {
+    document.querySelector('aside').classList.remove('view')
+})
